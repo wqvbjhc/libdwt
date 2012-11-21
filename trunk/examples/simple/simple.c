1060419d5f7ad6ff21ad18313a3b677161b76328
dabler@gmail.com <dabler@gmail.com@86e9e395-8442-93c2-020e-662a5c3994ba>
2012-11-21 19:04:35 +0000

    imported libdwt-2012-11-21-dev.zip

--- trunk/examples/simple/simple.c
+++ trunk/examples/simple/simple.c
@@ -8,19 +8,25 @@
 
 int main()
 {
+	// init platform
 	dwt_util_init();
 
+	// some log messages
 	dwt_util_log(LOG_INFO, "Library version is \"%s\".\n", dwt_util_version());
-	dwt_util_log(LOG_INFO, "We are running on \"%s\".\n", dwt_util_arch());
+	dwt_util_log(LOG_INFO, "We are running on \"%s\" architecture.\n", dwt_util_arch());
+	dwt_util_log(LOG_INFO, "Node name is \"%s\".\n", dwt_util_node());
+	dwt_util_log(LOG_INFO, "Application name is \"%s\".\n", dwt_util_appname());
 	dwt_util_log(LOG_INFO, "Using %i threads.\n", dwt_util_get_num_threads());
 	dwt_util_log(LOG_INFO, "Using %i workers.\n", dwt_util_get_num_workers());
 
 	const int x = 512, y = 512;
 
-	dwt_util_log(LOG_INFO, "size of %ix%i pixels\n", x, y);
+	dwt_util_log(LOG_INFO, "Using image of size of %ix%i pixels.\n", x, y);
 
 	const int stride_x = x * sizeof(float), stride_y = sizeof(float);
 	void *data1, *data2;
+
+	// full decomposition
 	int j = -1;
 
 	dwt_util_log(LOG_INFO, "generating test images...\n");
@@ -65,13 +71,16 @@ int main()
 		dwt_util_log(LOG_INFO, "images differs\n");
 	else
 		dwt_util_log(LOG_INFO, "success\n");
-
+	
+	// release platform resources
 	dwt_util_finish();
 
+	// save images into files
 	dwt_util_log(LOG_INFO, "saving...\n");
 	dwt_util_save_to_pgm_s("data1.pgm", 1.0, data1, stride_x, stride_y, x, y);
 	dwt_util_save_to_pgm_s("data2.pgm", 1.0, data2, stride_x, stride_y, x, y);
 
+	// free allocated memory
 	dwt_util_free_image(&data1);
 	dwt_util_free_image(&data2);
 
